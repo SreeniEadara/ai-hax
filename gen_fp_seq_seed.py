ade1dbad259d33221d99eb8986b9bc0d1e5a8271
sreenieadara <sreeni.eadara@gmail.com>
2023-06-17 23:53:14 -0700

    Parsing of seed sequences now works, todos remain

---
+++ gen_fp_seq_seed.py
@@ -0,0 +1,84 @@
+import openai
+import requests
+from Bio.Seq import Seq
+from Bio import AlignIO
+#from Bio.Alphabet import *
+
+def generate_fp_sequence(prompt:str):
+
+    OPENAI_API_KEY = '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'  # Replace with your OpenAI key
+    openai.api_key = OPENAI_API_KEY
+
+    messages = [
+        {"role": "system", "content": "For the following protein, create a .yml file containing a single protein per protein domain with the following format. - domain: type: name: pfam:"},
+        {"role": "user", "content": prompt}
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        max_tokens=512,
+        temperature=0,
+        messages=messages
+    )
+
+    pfam_values = []
+    choices = response["choices"]
+    for choice in choices:
+        message = choice["message"]
+        content = message["content"]
+        yaml_data = content.split("\n")[3:]
+        for line in yaml_data:
+            if "pfam" in line:
+                try:
+                    pfam_value = line.split(": ")[1]
+                    pfam_values.append(pfam_value)
+                except IndexError:
+                    print("Warning: Unexpected data format. Skipping.")
+                    
+
+    url = "https://www.ebi.ac.uk/interpro/api/entry/pfam/"
+
+    # Create a set of unique pfam_values to prevent duplicate requests
+    pfam_values = list(set(pfam_values))
+    data_list = []
+    sequences_list = []
+    protein_seed_sequences = []
+    protein_sequences = []
+    for pfam_value in pfam_values:
+        response = requests.get(url + pfam_value + "?annotation=alignment:seed")
+        if response.status_code == 200:
+            with open('tmp.txt', 'w') as f:
+                f.write(response.text)
+            align = AlignIO.read('tmp.txt', "stockholm")
+            for record in align:
+                protein_seed_sequences.append(record.seq)
+                #print(record.seq)
+            #data_list.append(sequence_response["probs_arr"])
+            #print(align)
+            
+            #TODO: input actual selection alg for mapped macro-seed
+            protein_sequences.append(protein_seed_sequences[0])
+        else:
+            print(f"Warning: Failed to retrieve data for pfam_value {pfam_value}.")
+
+    #print()
+
+    fusion_prot = ""
+
+    for protein_sequence in protein_sequences:
+        fusion_prot+=protein_sequence + "GSGSGS"
+
+    fusion_prot = fusion_prot[0:fusion_prot.rfind("GSGSGS")]
+
+    if fusion_prot[0] != "M":
+        fusion_prot = "M" + fusion_prot
+    
+    fusion_prot_seq = Seq(fusion_prot)
+    return(fusion_prot_seq)
+
+def run():
+    prompt = input('Provide comma-seprated list of keywords\n')
+    result = generate_fp_sequence(prompt)
+    print(result)
+
+run()
